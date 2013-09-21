7968d5a35a6a7c858328e853a4b49a980d4ca291
Michal Špaček <tupinek@gmail.com>
2013-09-21 18:22:28 +0200

    Prepare for 0.03 version.

--- Generator.pm
+++ Generator.pm
@@ -19,7 +19,7 @@ Readonly::Scalar our $YEAR_FROM => 1855;
 Readonly::Scalar our $YEAR_TO => 2054;
 
 # Version.
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 # Constructor.
 sub new {
@@ -357,6 +357,6 @@ BSD license.
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
--- t/Person-ID-CZ-RC-Generator/01-version.t
+++ t/Person-ID-CZ-RC-Generator/01-version.t
@@ -8,4 +8,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($Person::ID::CZ::RC::Generator::VERSION, 0.02, 'Version.');
+is($Person::ID::CZ::RC::Generator::VERSION, 0.03, 'Version.');
