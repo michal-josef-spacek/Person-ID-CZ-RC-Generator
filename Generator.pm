c39c4fa88334cedb6b9721c9c8456cbb7b31aae0
Michal Špaček <tupinek@gmail.com>
2013-11-12 20:12:35 +0100

    Prepare for 0.04 version.

--- Generator.pm
+++ Generator.pm
@@ -19,7 +19,7 @@ Readonly::Scalar our $YEAR_FROM => 1855;
 Readonly::Scalar our $YEAR_TO => 2054;
 
 # Version.
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -357,6 +357,6 @@ BSD license.
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
